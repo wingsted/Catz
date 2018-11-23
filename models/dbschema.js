fd55c03348512ff9d10b2b486460b87ba6bac163
wingsted <wingsted93@gmail.com>
2018-11-23 11:34:19 +0100

    user authentication

---
+++ models/dbschema.js
@@ -0,0 +1,32 @@
+/*var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;*/
