373ebf4884bc2349f2e351b6f7954797736ca6c8
mikkelu <mu@menly.io>
2018-11-27 09:57:08 +0100

    comments

--- middleware/guardMiddleware.js
+++ middleware/guardMiddleware.js
@@ -1,4 +1,4 @@
-// a middleware
+// a middleware that protects endpoints from not being logged in
 function guardMiddleware(req, res, next) {
     if (req.session && req.session.userID) {
         return next();
